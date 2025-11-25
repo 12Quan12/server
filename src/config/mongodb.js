ee20f73a0f9352ed7e6fbff3314ee181d322d6d7
12Quan12 <116006961+12Quan12@users.noreply.github.com>
2025-11-25 20:27:39 +0700

    server

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,3 +3,36 @@
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+
+import { MongoClient, ServerApiVersion } from "mongodb"
+import { env } from "~/config/environment"
+
+// khởi tạo đối tượng databaseInstace ban đầu là null vì chưa connect
+let dataBaseInstace = null
+
+// Khởi tạo 1 đối tượng Instace để kết nối tới mongoDB
+const mongoClientInstace = new MongoClient(env.MONGODB_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1, // 
+        strict: true,
+        deprecationErrors: true
+    }
+})
+
+// kết nối tới database
+export const CONNECT_DB = async () => {
+    // gọi kết nối tới mongoDB
+    await mongoClientInstace.connect()
+
+    // kết nối thành công thì gán vào dataBaseInstace
+    dataBaseInstace = await mongoClientInstace.db(env.DATABASE_NAME)
+}
+
+export const CLOSE_DB = async () => {
+    await mongoClientInstace.close()
+}
+
+export const GET_DB = () => {
+    if (!dataBaseInstace) throw new Error("Must connect to Database first!!!")
+    return dataBaseInstace
+}
